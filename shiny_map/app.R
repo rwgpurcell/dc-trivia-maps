0d8dfaa14df477b7d279a1aa0f1708fc953eae20
Rob Purcell <rwgpurcell@gmail.com>
2019-03-21 13:18:48 -0400

    add labels and popups to trivia markers

--- shiny_map/app.R
+++ shiny_map/app.R
@@ -11,16 +11,21 @@ DATA_DIR <- file.path("../")
 venueData <- read_csv(file.path(DATA_DIR,"trivia_venues_loc.csv"))
 
 print(head(venueData))
+days <- c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+days <- days[days %in% unique(venueData$Day)]
+print(days)
+
 r_colors <- rgb(t(col2rgb(colors()) / 255))
 names(r_colors) <- colors()
 
 
 ui <- fluidPage(
+  fluidRow(h1('District Trivia Venue Map')),
   fluidRow(
-    column(3,
-           actionButton("recalc", "New points")
+    column(2,
+           checkboxGroupInput('daySelect',"Trivia Day",choices=days,selected = days)
     ),
-    column(9,
+    column(10,
       leafletOutput("mymap", height=800),
       p()
     )
@@ -29,16 +34,14 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   
-  points <- eventReactive(input$recalc, {
-    cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  }, ignoreNULL = FALSE)
   
   output$mymap <- renderLeaflet({
-    leaflet(height = "600px") %>%
+    leaflet(data = venueData) %>%
       addProviderTiles(providers$Stamen.TonerLite,
                        options = providerTileOptions(noWrap = TRUE)
       ) %>%
-      addMarkers(data = venueData)
+      addMarkers(label=~Venue,
+                 popup=paste(venueData$Venue,"<br>",venueData$Day,"at",venueData$Time))
   })
 }
 
